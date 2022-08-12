9368886261627c7b741381f6fad56541bf3ca0ea
jf-gbg <malmmlam@outlook.com>
2022-08-12 13:09:48 +0200

    complete chapter 3 exercises

--- Chapter3/main.cpp
+++ Chapter3/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <string>
+using std::string;
 
 int main() {
+    string s1, s2;
+
+    while (cin >> s1) {
+        s2 += s1 + " ";
+        cout << s2 << endl;
+    }
 
     return 0;
 }
