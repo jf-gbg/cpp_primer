75f670243dae6ca27caa0adb8877d9962b27789c
jf-gbg <malmmlam@outlook.com>
2022-08-14 12:04:17 +0200

    complete exercise 3.12

--- Chapter3/main.cpp
+++ Chapter3/main.cpp
@@ -9,21 +9,11 @@ using std::string;
 #include <cctype>
 using std::isspace;
 
+#include <vector>
+using std::vector;
+
 int main() {
-    string s = "h!e,,.ll;/o";
-    string sOut = "";
 
-    for (auto &c : s) {
-        if (!ispunct(c))
-            sOut += c;
-    }
-
-//    for (decltype(s.size()) index = 0; index < s.length(); index++) {
-//        if (!ispunct(s[index]))
-//            sOut += s[index];
-//    }
-
-    cout << sOut << endl;
 
     return 0;
 }
