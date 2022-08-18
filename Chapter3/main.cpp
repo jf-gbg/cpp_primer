517f0bf3eac8eb4e7be62b09e1d488d066ec053e
jf-gbg <malmmlam@outlook.com>
2022-08-18 11:54:55 +0200

    complete exercise 3.30

--- Chapter3/main.cpp
+++ Chapter3/main.cpp
@@ -17,26 +17,6 @@ int ia[10];
 
 int main() {
 
-    string sa2[10];
-    string ia2[10];
-
-    cout<< "sa" << endl;
-    for (auto s : sa) {
-        cout << s << endl;
-    }
-    cout << "ia" << endl;
-    for (auto i : ia) {
-        cout << i << endl;
-    }
-    cout << "sa2" << endl;
-    for (auto s : sa2) {
-        cout << s << endl;
-    }
-    cout << "ia2" << endl;
-    for (auto i : ia2) {
-        cout << i << endl;
-    }
-    return 0;
 }
 
 //42
