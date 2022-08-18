531d15631ee79851909b1fc7cc549c88e4e6089c
jf-gbg <malmmlam@outlook.com>
2022-08-18 11:59:19 +0200

    complete exercise 3.31

--- Chapter3/3.31/3.31.cpp
+++ Chapter3/3.31/3.31.cpp
@@ -3,9 +3,6 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-#include <string>
-using std::string;
-
 #include <cctype>
 using std::isspace;
 
@@ -15,7 +12,7 @@ using std::vector;
 string sa[10];
 int ia[10];
 
-int main() {
+int exercise331() {
     constexpr size_t arraySize = 10;
     int ia[arraySize];
 
@@ -29,18 +26,3 @@ int main() {
     cout << endl;
 
 }
-
-//42
-//65
-//95
-//100
-//39
-//67
-//95
-//76
-//88
-//76
-//83
-//92
-//76
-//93
