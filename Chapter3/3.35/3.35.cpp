a5a9616419e19fd79bd922e424475d38690a4be7
jf-gbg <malmmlam@outlook.com>
2022-08-18 14:39:52 +0200

    complete exercise 3.35

--- Chapter3/3.35/3.35.cpp
+++ Chapter3/3.35/3.35.cpp
@@ -5,23 +5,23 @@ using std::endl;
 using std::begin;
 using std::end;
 
-#include <string>
-using std::string;
-
 #include <cctype>
 using std::isspace;
 
 #include <vector>
 using std::vector;
 
+string sa[10];
+int ia[10];
+
 int main() {
-   int ia[] = {0,2,4,6,8};
-   int *beg = begin(ia);
-   int *last = end(ia);
-   while(beg != last) {
-       *beg = 0;
-       beg++;
-   }
+    int ia[] = {0,2,4,6,8};
+    int *beg = begin(ia);
+    int *last = end(ia);
+    while(beg != last) {
+        *beg = 0;
+        beg++;
+    }
 
     for (auto i:ia) {
         cout << i << " ";
